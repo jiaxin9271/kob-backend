b6ac9de74497f3e9f39b0474513faa0bd456ad9c
chenjiaxin <chenjx529@qq.com>
2024-01-29 02:25:24 +0800

    login

--- src/main/java/com/kob/backend/service/impl/user/account/LoginServiceImpl.java
+++ src/main/java/com/kob/backend/service/impl/user/account/LoginServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -23,16 +24,17 @@ public class LoginServiceImpl implements LoginService {
     public Map<String, String> getToken(String username, String password) {
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(username, password);
-
-        Authentication authenticate = authenticationManager.authenticate(authenticationToken);  // 登录失败，会自动处理
-        UserDetailsImpl loginUser = (UserDetailsImpl) authenticate.getPrincipal();  // 把用户取出来
-        User user = loginUser.getUser();
-        String jwt = JwtUtil.createJWT(user.getId().toString());
-
         Map<String, String> map = new HashMap<>();
-        map.put("error_message", "success");
-        map.put("token", jwt);
-
+        try {
+            Authentication authenticate = authenticationManager.authenticate(authenticationToken);
+            UserDetailsImpl loginUser = (UserDetailsImpl) authenticate.getPrincipal();
+            User user = loginUser.getUser();
+            String jwt = JwtUtil.createJWT(user.getId().toString());
+            map.put("error_message", "success");
+            map.put("token", jwt);
+        } catch (AuthenticationException e) {
+            map.put("error_message", "密码错误");
+        }
         return map;
     }
 }
